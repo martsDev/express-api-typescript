48980cc4af60ef16558d2ec04f04ca031a3461d1
martsDev <martssilvapro@gmail.com>
2022-08-09 15:45:05 -0300

    setup app

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.status(StatusCodes.OK).send('Hello World!'));
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
