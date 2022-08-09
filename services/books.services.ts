299ac5624eeee9547e9524da148be34ddce6c8ae
martsDev <martssilvapro@gmail.com>
2022-08-09 16:46:40 -0300

    implements books.services.ts

---
+++ services/books.services.ts
@@ -0,0 +1,19 @@
+import connection from "../models/connection";
+import BookModel from "../models/book.model";
+import Book from "../interfaces/book.interface";
+
+class BookService {
+    public model: BookModel;
+
+    constructor() {
+        this.model = new BookModel(connection);
+    };
+
+    public async getAll(): Promise<Book[]> {
+        const books = await this.model.getAll();
+
+        return books;
+    };
+};
+
+export default BookService;
