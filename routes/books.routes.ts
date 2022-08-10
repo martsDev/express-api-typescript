645a49183390f1372e9d128c0a3fb3b661acdd42
martsDev <martssilvapro@gmail.com>
2022-08-10 17:12:59 -0300

    implements delete route

--- routes/books.routes.ts
+++ routes/books.routes.ts
@@ -6,9 +6,12 @@ const router = Router();
 
 const booksController = new BooksController();
 
+const bookSlashId = '/books/:id';
+
 router.get('/books', booksController.getAll);
-router.get('/books/:id', booksController.getById);
+router.get(bookSlashId, booksController.getById);
 router.post('/books/', validationBook, booksController.create);
-router.put('/books/:id', validationBook, booksController.updateBook);
+router.put(bookSlashId, validationBook, booksController.updateBook);
+router.delete(bookSlashId, booksController.removeBook);
 
 export default router;
